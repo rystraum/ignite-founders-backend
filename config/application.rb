73eedea69993e52adf88f61e01b3022425b6434e
Rystraum Gamonez <rystraum@gmail.com>
2017-08-05 07:31:16 +0800

    rack-cors

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ module IgniteJourneyRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.insert_after Rails::Rack::Logger, Rack::Cors, :debug => true, :logger => Rails.logger do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i(get post put patch delete options head)
+      end
+    end
   end
 end
