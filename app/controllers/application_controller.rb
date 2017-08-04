fa61c82b7a140c740c2282e7be32288b1fb5d4f2
Rystraum Gamonez <rystraum@gmail.com>
2017-08-05 07:25:37 +0800

    Users routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def homepage
+  def users
+    @users = User.all
+    render json: @users
+  end
+
+  def create_user
+    @user = User.create params.permit(:first_name, :last_name, :email, :birth_date)
+    render json: @user
+  end
+
+  def update_user
+    @user = User.find(params[:id])
+    @user.update_attributes(data: params[:data])
+
+    render json: @user
   end
 end
