89a4d5cc3718257742a83a1f0780853757f6f0a4
Rystraum Gamonez <rystraum@gmail.com>
2017-08-05 09:24:43 +0800

    Do not create new user if email is already found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_filter :verify_authenticity_token
 
-
   def users
     @users = User.all
     render json: @users
   end
 
   def create_user
-    @user = User.create params.permit(:first_name, :last_name, :email, :birth_date)
+    user = User.find_by(email: params[:email])
+    @user = user || User.create(params.permit(:first_name, :last_name, :email, :birth_date))
     render json: @user
   end
 
