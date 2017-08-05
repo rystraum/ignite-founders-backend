8c85068d786b0a96bcf8e8b9e58e151fea85ab2d
Rystraum Gamonez <rystraum@gmail.com>
2017-08-05 08:12:53 +0800

    Skip CSRF check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_before_filter :verify_authenticity_token
+
 
   def users
     @users = User.all
